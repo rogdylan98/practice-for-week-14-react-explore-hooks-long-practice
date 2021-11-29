a0cb8643fdd5e79f8e6c4bbd7ea5894c04b5757b
Tommy Forde <tommyforde@Tommys-MacBook-Pro.local>
2021-11-29 15:12:23 -0600

    finished bonus phase

--- src/components/ProductListItem/ProductListItem.js
+++ src/components/ProductListItem/ProductListItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 function ProductListItem({ product, isSelected, onClick }) {
-    return (
-        <div className={`product-list-item ${isSelected ? ' selected' : ''}`}>
-            <img className="product-list-item-photo"
-                 src={product.photo.filename}
-                 alt={`${product.name}`}
-            />
-            <button onClick={onClick}>{product.name}</button>
-        </div>
-    )
+  console.log('product list item 4') 
+  return (
+    <div className={`product-list-item ${isSelected ? ' selected' : ''}`}>
+        <img className="product-list-item-photo"
+             src={product.photo.filename}
+             alt={`${product.name}`}
+        />
+        <button onClick={onClick}>{product.name}</button>
+    </div>
+  )
 }
 
 export default ProductListItem;
--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,43 +1,63 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
 
 function ProductView({ products }) {
+  const [sideOpen, setSideOpen] = useState(
+    JSON.parse(localStorage.getItem('sideOpen')) || false
+  );
+  const [product, setProduct] = useState(
+    JSON.parse(localStorage.getItem('product')) || ''
+  );
+  
+  useEffect(() => {
+    if (product) localStorage.setItem('product', JSON.stringify(product));
+    if (sideOpen) localStorage.setItem('sideOpen', JSON.stringify(sideOpen));
+  }, [product, sideOpen]);
+  
+  useEffect(() => {
+    if (product) {
+      setSideOpen(true);
+    }
+  }, [product]);
 
-    // TODO: Replace with state variable
-    const [sideOpen, setSideOpen] = useState(true);
-    const [product, setProduct] = useState('')
-    // const sideOpen = true;
+  useEffect(() => {
+    if (!sideOpen) {
+      setProduct();
+    }
+  }, [sideOpen]);
 
-    return (
-        <div className="product-view">
-            <div className="product-main-area">
-                <h1>Products</h1>
-                <div className="product-list">
-                    {products.map(item =>
-                        <ProductListItem
-                            key={item.id}
-                            product={item}
-                            onClick={() => setProduct(item)}
-                            isSelected={product ? product.id === item.id : false}
-                        />
-                    )}
-                </div>
-            </div>
-            <div className="product-side-panel">
-                <div className="product-side-panel-toggle-wrapper">
-                    <div className="product-side-panel-toggle"
-                         onClick={() => setSideOpen(!sideOpen)}>
-                        {sideOpen ? '>' : '<'}
-                    </div>
-                </div>
-                <ProductDetails
-                visible={sideOpen}
-                product={product} />
+  console.log('product view 22');
+  
+  return (
+    <div className="product-view">
+        <div className="product-main-area">
+            <h1>Products</h1>
+            <div className="product-list">
+                {products.map(item =>
+                    <ProductListItem
+                        key={item.id}
+                        product={item}
+                        onClick={() => setProduct(item)}
+                        isSelected={product ? product.id === item.id : false}
+                    />
+                )}
             </div>
         </div>
-    );
+        <div className="product-side-panel">
+            <div className="product-side-panel-toggle-wrapper">
+                <div className="product-side-panel-toggle"
+                     onClick={() => setSideOpen(!sideOpen)}>
+                    {sideOpen ? '>' : '<'}
+                </div>
+            </div>
+            <ProductDetails
+            visible={sideOpen}
+            product={product} />
+        </div>
+    </div>
+  );
 }
 
 export default ProductView;
